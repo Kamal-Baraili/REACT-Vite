6e6d9bc68e93df17dd919bfa6ece2ba48bb214a6
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-23 13:09:51 +0545

    Ternary Operators in React

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  const age = 18;
+  const isGreen = true;
 
   return (
     <div className="container">
-      <h1>sdfsdfsdf</h1>
+      <h1 className="name">Kamal</h1>
+      {age >= 18 ? (
+        <h2 style={{ color: "green" }}>Eligible</h2>
+      ) : (
+        <h2 style={{ color: "red" }}>Not Eligible</h2>
+      )}
+
+      {isGreen && <button>Login</button>}
     </div>
   );
 }

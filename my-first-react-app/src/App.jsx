34a4d04d7c612ee9b6a84995bd5a37a8d32bbd31
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-23 12:50:43 +0545

    CSS in react

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
-      <h1 className="name">Kamal</h1>
+      <h1>sdfsdfsdf</h1>
     </div>
   );
 }

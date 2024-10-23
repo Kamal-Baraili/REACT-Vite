8a464ff3f8b1f33d60a8e38c33d7e4cd26339f26
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-23 13:33:31 +0545

    Lists in REACT

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,20 +1,36 @@
 import "./App.css";
+import Users from "./Users";
 
 function App() {
   const age = 18;
   const isGreen = true;
+  const names = ["Kamal", "John", "Mark", "Jennifer", "Mike"];
+  const users = [
+    { name: "Kamal", age: "25" },
+    { name: "John", age: "22" },
+    { name: "Mark", age: "23" },
+  ];
 
   return (
-    <div className="container">
-      <h1 className="name">Kamal</h1>
-      {age >= 18 ? (
-        <h2 style={{ color: "green" }}>Eligible</h2>
-      ) : (
-        <h2 style={{ color: "red" }}>Not Eligible</h2>
-      )}
+    <>
+      <div className="container">
+        <h1 className="name">Kamal</h1>
+        {age >= 18 ? (
+          <h2 style={{ color: "green" }}>Eligible</h2>
+        ) : (
+          <h2 style={{ color: "red" }}>Not Eligible</h2>
+        )}
 
-      {isGreen && <button>Login</button>}
-    </div>
+        {isGreen && <button>Login</button>}
+
+        {names.map((nam, key) => {
+          return <h3 key={key}>{nam}</h3>;
+        })}
+      </div>
+      {users.map((user, key) => {
+        return <Users name={user.name} age={user.age} />;
+      })}
+    </>
   );
 }
 
---
+++ my-first-react-app/src/Users.jsx
@@ -0,0 +1,9 @@
+const Users = (props) => {
+  return (
+    <div>
+      {props.name} {props.age}
+    </div>
+  );
+};
+
+export default Users;
